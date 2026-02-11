5b9fbd3bdbacef89e719985c76dc9d09fc45fe71
Sujit Khalkar <sujitkhalkar28@gmail.com>
2026-02-11 17:49:08 +0530

    fix allowed hosts

--- Resturant_Project/settings.py
+++ Resturant_Project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!hkm^2bf%*q1m+add*#t571u0@-wp#1x0#9a0hl&=s(wfar=)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
